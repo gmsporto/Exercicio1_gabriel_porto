bc8769e215bf4a9cf42702a88e9d288f66f3b21c
gmsporto <76124857+gmsporto@users.noreply.github.com>
2021-01-06 16:20:09 -0300

    Aula 20210106

--- fun_exercicio.cpp
+++ fun_exercicio.cpp
@@ -19,6 +19,31 @@ v[1] = norm(0, 2.5);
 v[2] = v[0]+v[1];
 RESULT(v[2])
 
+EQUATION("X_Sum")
+/*
+Variável do Setor
+Equivalente macro: RESULT(SUM("X"))
+*/
+v[0]=0;
+CYCLE(cur, "FIRM")
+{
+		v[1]=VS(cur,"X");
+		v[0]=v[0]+v[1];
+}
+RESULT(v[0])
+
+EQUATION("X_Ave")
+/*
+Variável do Setor
+*/
+RESULT(AVE("X"))
+
+EQUATION ("X_Max")
+/*
+Variável do Setor
+*/
+RESULT(MAX("X"))
+
 
 
 MODELEND
