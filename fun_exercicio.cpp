21cc8f4b3dffea30f5602b3dfab956e6ac71c536
gmsporto <76124857+gmsporto@users.noreply.github.com>
2021-01-11 20:51:57 -0300

    Exercicio 2
    Resultado do Exercicio 2.

--- fun_exercicio.cpp
+++ fun_exercicio.cpp
@@ -12,17 +12,17 @@ MODELBEGIN
 
 EQUATION("X")
 /*
-X (t) = X (t-1) + ruido branco   
+LEVEL: FIRM
 */
 v[0] = VL("X",1);
-v[1] = norm(0, 2.5); 
-v[2] = v[0]+v[1];
-RESULT(v[2])
+v[1] = V("Max"); 
+v[2] = V("Min");
+v[3] = v[0]+uniform(v[1],v[2]);
+RESULT(v[3])
 
 EQUATION("X_Sum")
 /*
-Variável do Setor
-Equivalente macro: RESULT(SUM("X"))
+LEVEL: SECTOR
 */
 v[0]=0;
 CYCLE(cur, "FIRM")
@@ -34,16 +34,72 @@ RESULT(v[0])
 
 EQUATION("X_Ave")
 /*
-Variável do Setor
+LEVEL: SECTOR
 */
-RESULT(AVE("X"))
+v[0]=0;
+CYCLE(cur, "FIRM")
+{
+		v[1]=VS(cur,"X");
+		v[0]=v[0]+v[1];
+		v[2]= COUNT_ALLS(cur,"FIRM");  
+}
+RESULT(v[0]/v[2])
+
 
 EQUATION ("X_Max")
 /*
-Variável do Setor
+LEVEL: SECTOR
 */
-RESULT(MAX("X"))
+v[0]=0;
+CYCLE(cur, "FIRM")
+{
+		v[1]=VS(cur, "X");
+		if (v[1]>v[0])   
+		{v[0]=v[1];}
+		else 
+		{v[0]=v[0];}
+}
+RESULT(v[0])
 
+
+EQUATION("MktSh")
+/*
+LEVEL: FIRM
+*/
+v[0]=V("X");
+v[1]=V("X_Sum");
+RESULT(v[0]/v[1])
+
+
+EQUATION("Sum_MktSh")
+/*
+LEVEL:SECTOR
+*/
+v[0]=0;
+CYCLE(cur,"FIRM")
+{
+		v[1]=VS(cur,"MktSh");
+		v[0]=v[0]+v[1];
+}
+RESULT(v[0])
+
+
+EQUATION("Leader") // DESAFIO
+/*
+LEVEL: SECTOR
+*/
+v[0]=0;
+v[1]=0;
+CYCLE(cur, "FIRM")
+{
+		v[2]=VS(cur, "X");
+		v[3]=VS(cur,"IdFirm");
+		if (v[2]>v[0])   
+		{v[0]=v[2]; v[1]=v[3];}
+		else 
+		{v[0]=v[0]; v[1]=v[1];}
+}
+RESULT(v[1]) 
 
 
 MODELEND
