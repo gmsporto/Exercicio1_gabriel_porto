70f8f2009b988ed05e97ae79c56264116a91a7ed
gmsporto <76124857+gmsporto@users.noreply.github.com>
2021-01-20 12:28:48 -0300

    Exercicio 2.1
    Tentativa.

--- fun_exercicio.cpp
+++ fun_exercicio.cpp
@@ -10,97 +10,124 @@ MODELBEGIN
 // insert your equations here, between the MODELBEGIN and MODELEND words
 
 
-EQUATION("X")
+EQUATION("X") //Sim1
 /*
 LEVEL: FIRM
 */
-v[0] = VL("X",1);
-v[1] = V("Max"); 
-v[2] = V("Min");
-v[3] = v[0]+uniform(v[1],v[2]);
-RESULT(v[3])
+	v[0] = VL("X",1);
+	v[1] = V("Max"); 
+	v[2] = V("Min");
+	v[3] = v[0]+uniform(v[1],v[2]);
+	RESULT(v[3])
 
-EQUATION("X_Sum")
+EQUATION("X_Sum")  //Sim2
 /*
 LEVEL: SECTOR
 */
-v[0]=0;
-CYCLE(cur, "FIRM")
-{
-		v[1]=VS(cur,"X");
-		v[0]=v[0]+v[1];
-}
-RESULT(v[0])
+	v[0]=0;
+	CYCLE(cur, "FIRM")
+	{
+			v[1]=VS(cur,"X");
+			v[0]=v[0]+v[1];
+	}
+	RESULT(v[0])
 
 EQUATION("X_Ave")
 /*
 LEVEL: SECTOR
 */
-v[0]=0;
-CYCLE(cur, "FIRM")
-{
-		v[1]=VS(cur,"X");
-		v[0]=v[0]+v[1];
-		v[2]= COUNT_ALLS(cur,"FIRM");  
-}
-RESULT(v[0]/v[2])
+	v[0]=0;
+	CYCLE(cur, "FIRM")
+	{
+			v[1]=VS(cur,"X");
+			v[0]=v[0]+v[1];
+			v[2]= COUNT_ALLS(cur,"FIRM");  
+	}
+	RESULT(v[0]/v[2])
 
 
 EQUATION ("X_Max")
 /*
 LEVEL: SECTOR
 */
-v[0]=0;
-CYCLE(cur, "FIRM")
-{
-		v[1]=VS(cur, "X");
-		if (v[1]>v[0])   
-		{v[0]=v[1];}
-		else 
-		{v[0]=v[0];}
-}
-RESULT(v[0])
+	v[0]=0;
+	CYCLE(cur, "FIRM")
+	{
+			v[1]=VS(cur, "X");
+			if (v[1]>v[0])   
+			{v[0]=v[1];}
+			else 
+			{v[0]=v[0];}
+	}
+	RESULT(v[0])
 
 
-EQUATION("MktSh")
+EQUATION("MktSh") //Sim3
 /*
 LEVEL: FIRM
 */
-v[0]=V("X");
-v[1]=V("X_Sum");
-RESULT(v[0]/v[1])
+	v[0]=V("X");
+	v[1]=V("X_Sum");
+	RESULT(v[0]/v[1])
 
 
 EQUATION("Sum_MktSh")
 /*
 LEVEL:SECTOR
 */
-v[0]=0;
-CYCLE(cur,"FIRM")
-{
-		v[1]=VS(cur,"MktSh");
-		v[0]=v[0]+v[1];
-}
-RESULT(v[0])
+	v[0]=0;
+	CYCLE(cur,"FIRM")
+	{
+			v[1]=VS(cur,"MktSh");
+			v[0]=v[0]+v[1];
+	}
+	RESULT(v[0])
 
 
 EQUATION("Leader") // DESAFIO
 /*
 LEVEL: SECTOR
 */
-v[0]=0;
-v[1]=0;
-CYCLE(cur, "FIRM")
-{
-		v[2]=VS(cur, "X");
-		v[3]=VS(cur,"IdFirm");
-		if (v[2]>v[0])   
-		{v[0]=v[2]; v[1]=v[3];}
-		else 
-		{v[0]=v[0]; v[1]=v[1];}
-}
-RESULT(v[1]) 
+	v[0]=0;
+	v[1]=0;
+	CYCLE(cur, "FIRM")
+	{
+			v[2]=VS(cur, "X");
+			v[3]=VS(cur,"IdFirm");
+			if (v[2]>v[0])   
+			{v[0]=v[2]; v[1]=v[3];}
+			else 
+			{v[0]=v[0]; v[1]=v[1];}
+	}
+	RESULT(v[1]) 
 
+
+EQUATION("Rank") // Sim4
+/*
+LEVEL: SECTOR
+*/
+	v[0]=1;
+	CYCLE(cur, "FIRM")
+	{
+			cur1=SORT("FIRM", "X", "DOWN");
+			CYCLE(cur, cur1)
+			{
+					WRITES(cur, "firm_rank", v[0]);
+					v[0]=v[0]+1;	
+			}				
+	}
+
+/*
+v[0]=1;
+	cur1=SORT("FIRM", "X", "DOWN");
+	CYCLE(cur, cur1)
+	{
+			WRITES(cur, "firm_rank", v[0]);
+			v[0]=v[0]+1;				
+	}
+*/
+
+RESULT(v[0])
 
 MODELEND
 
